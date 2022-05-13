ef34f8615ec4035363c62d5f6ad677e5e01be8da
lashla <lash.la412@gmail.com>
2022-05-13 18:22:32 +0300

    cannot find the solution on how to open the file

--- app/src/main/java/com/lasha/csvtoobj/MainViewModel.kt
+++ app/src/main/java/com/lasha/csvtoobj/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.lasha.csvtoobj
 
-import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,32 +9,31 @@ import jcifs.CIFSContext
 import jcifs.context.SingletonContext
 import jcifs.smb.SmbFile
 import jcifs.smb.SmbFileInputStream
-import jcifs.smb.SmbFileOutputStream
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.File
-import java.io.FileInputStream
+import java.net.URL
+import java.nio.file.Paths
 
 
 class MainViewModel: ViewModel() {
-    val lineLiveData =  MutableLiveData<ArrayList<List<String>>>()
+    val lineLiveData =  MutableLiveData<List<List<String>>>()
 
     private fun getFile(){
-
-        var linesData = ArrayList<List<String>>()
+        var linesData = emptyList<List<String>>()
             viewModelScope.launch(Dispatchers.IO) {
-                val base: CIFSContext = SingletonContext.getInstance()
+//                val base: CIFSContext = SingletonContext.getInstance()
 
                 val dir = SmbFile("smb://192.168.1.9/shared/")
                 Log.i("DIR", dir.path.toString())
                 Log.i("Directory files", dir.listFiles()[0].name)
-                val file = SmbFile("smb://192.168.1.9/shared/roman.csv/")
-                val smbFileInput = SmbFileInputStream(file)
-
-//                linesData = csvReader().readAll(File) as ArrayList<List<String>>
+                val url = URL("smb://192.168.1.9/shared/roman.csv")
+                val file: String? = url.file
+                linesData = csvReader().readAll(file!!)
+                Log.i("Lines", "${linesData[0]}")
             }
+        lineLiveData.value = linesData
 
-            lineLiveData.value = linesData
 
     }
     init {
