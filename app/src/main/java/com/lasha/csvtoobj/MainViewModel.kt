4b8b043c3427a02f50ffb6313502c7d5cef9a469
lashla <lash.la412@gmail.com>
2022-05-13 17:57:14 +0300

    cannot find the solution on how to open the file

--- app/src/main/java/com/lasha/csvtoobj/MainViewModel.kt
+++ app/src/main/java/com/lasha/csvtoobj/MainViewModel.kt
@@ -1,35 +1,39 @@
 package com.lasha.csvtoobj
 
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import jcifs.CIFSContext
 import jcifs.context.SingletonContext
-import jcifs.smb.NtlmPasswordAuthentication
 import jcifs.smb.SmbFile
+import jcifs.smb.SmbFileInputStream
+import jcifs.smb.SmbFileOutputStream
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileInputStream
 
 
 class MainViewModel: ViewModel() {
-    val lineLiveData =  MutableLiveData<ArrayList<String>>()
+    val lineLiveData =  MutableLiveData<ArrayList<List<String>>>()
 
     private fun getFile(){
 
-        val linesData = ArrayList<String>()
+        var linesData = ArrayList<List<String>>()
             viewModelScope.launch(Dispatchers.IO) {
                 val base: CIFSContext = SingletonContext.getInstance()
-                val authed1 = base.withCredentials(
-                    NtlmPasswordAuthentication(
-                        base, "Alexey",
-                        "alexey", "xdfe29hg"
-                    )
-                )
+
                 val dir = SmbFile("smb://192.168.1.9/shared/")
                 Log.i("DIR", dir.path.toString())
                 Log.i("Directory files", dir.listFiles()[0].name)
-                linesData.add(dir.listFiles().toString())
+                val file = SmbFile("smb://192.168.1.9/shared/roman.csv/")
+                val smbFileInput = SmbFileInputStream(file)
+
+//                linesData = csvReader().readAll(File) as ArrayList<List<String>>
             }
 
             lineLiveData.value = linesData
