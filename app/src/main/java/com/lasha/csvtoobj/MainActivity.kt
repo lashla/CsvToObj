91d88eb18e2a963cb3c2af359edbae2729f0375c
lashla <lash.la412@gmail.com>
2022-05-23 19:34:41 +0300

    making intent work

--- app/src/main/java/com/lasha/csvtoobj/MainActivity.kt
+++ app/src/main/java/com/lasha/csvtoobj/MainActivity.kt
@@ -1,32 +1,28 @@
 package com.lasha.csvtoobj
 
-import android.Manifest
-import android.app.Activity
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.IntentFilter
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.wifi.p2p.WifiP2pDevice
-import android.net.wifi.p2p.WifiP2pDeviceList
-import android.net.wifi.p2p.WifiP2pManager
-import android.opengl.Visibility
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
-import android.view.KeyEvent
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.doyaaaaaken.kotlincsv.dsl.context.ExcessFieldsRowBehaviour
+import com.github.doyaaaaaken.kotlincsv.dsl.context.InsufficientFieldsRowBehaviour
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.*
-import java.net.InetAddress
-import kotlin.math.log
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,19 +34,94 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enterRoot()
+        setupOnClickListeners()
         initViewModel()
         initRecyclerView()
-
+        checkPermissions()
     }
 
-    private fun enterRoot(){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        if (resultCode == RESULT_OK){
+            val uri = resultData!!.data
+            val pathParent = resultData.data!!.path!!.replace("${resultData.data!!.lastPathSegment}", "")
+            Log.i("path parent", pathParent)
+//            val filePath = uri!!.path
+//            val uriToPath = contentResolver.openInputStream(uri)
+//            Log.i("Uri", uri)
+            val inputStream: InputStream? = contentResolver.openInputStream(uri!!)
+            val file = File.createTempFile("fileName", ".csv", this.cacheDir)
+            val outputStream = FileOutputStream(file)
+
+            val buffer = ByteArray(10000)
+            var bytesRead: Int
+
+            while (inputStream!!.read(buffer).also { bytesRead = it } != -1) {
+                outputStream.write(buffer, 0, bytesRead)
+            }
+            inputStream.close()
+            outputStream.close()
+
+            importCsv(file, pathParent)
+        }
+    }
+
+    private fun setupOnClickListeners(){
         searchPath.setOnClickListener {
             viewModel.takeFileContents("smb://"+ filePathEt.text.toString() + ".csv")
             exceptionTV.visibility = View.INVISIBLE
         }
+        openFileExplorerBtn.setOnClickListener {
+            chooseFileFromStorage()
+        }
     }
 
+    private fun chooseFileFromStorage(){
+        val PICK_CSV_FILE = 1
+
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "*/*"
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                putExtra(DocumentsContract.EXTRA_INITIAL_URI, "Open csv")
+            }
+            startActivityForResult(intent, PICK_CSV_FILE)
+    }
+
+    private fun importCsv(file: File, path: String){
+        val reader = csvReader{
+            charset = "Windows-1251"
+            delimiter = ';'
+            excessFieldsRowBehaviour = ExcessFieldsRowBehaviour.IGNORE
+            insufficientFieldsRowBehaviour = InsufficientFieldsRowBehaviour.IGNORE
+        }
+        val writer = csvWriter{
+            charset = "Windows-1251"
+        }
+
+        val fileInputStream = FileInputStream(file)
+        val csvOutput = reader.readAll(fileInputStream)
+        val timeStampPattern = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+        val newFileName = timeStampPattern.format(LocalDateTime.now())
+        showLocalCsv(csvOutput)
+//        val newFile = File("$path$newFileName.csv")
+//        if (newFile.exists()){
+//            val fileOutputStream = FileOutputStream(newFile)
+//            writer.writeAll(csvOutput, fileOutputStream)
+//            fileOutputStream.close()
+//        }
+
+        fileInputStream.close()
+
+    }
+
+    private fun showLocalCsv(csvOutput: List<List<String>>){
+        for (element in csvOutput){
+            recyclerView.visibility = View.VISIBLE
+            data.add(CsvData(element[0],element[1],element[2],element[3],element[4],element[5],element[6]))
+            adapter.updateDbInfo(data)
+        }
+    }
 
 
     private fun initViewModel() {
@@ -84,5 +155,29 @@ class MainActivity : AppCompatActivity() {
         recyclerView.itemAnimator = DefaultItemAnimator()
         recyclerView.adapter = adapter
     }
+
+    private fun checkPermissions(){
+        if (!allPermissionsGranted()){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                REQUEST_CODE_PERMISSIONS
+            );
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            this, it) == PackageManager.PERMISSION_GRANTED
+    }
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).apply {
+            }.toTypedArray()
+    }
+
 
 }
